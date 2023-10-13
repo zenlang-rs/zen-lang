a4e6745277349da3c3feed634def86e2b5aa9561
Lab Rat <dev.frolics@gmail.com>
2023-10-13 12:07:42 +0530

    wip{1:5} : fixed few bugs in transpilation

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,8 @@ pub enum Token {
     Number,
 
     Invalid(char),
+
+    Uninitialized,
 }
 
 const EOF: u8 = 0;
@@ -24,80 +26,90 @@ thread_local!(static NUM_VALUE: RefCell<f64> = RefCell::new(0.0)); // Holds curr
 thread_local!(static LAST_CHAR: RefCell<char> = RefCell::new(' '));
 
 pub fn get_tok() -> Token {
-    let mut detect_token = Token::Invalid(' ');
+    let mut last_char_dup = ' ';
+    let mut detect_token;
     LAST_CHAR.with(|l_char| {
         let mut last_char = l_char.borrow_mut();
         while last_char.is_whitespace() {
             *last_char = input::get_char();
         }
-        detect_token = IDENTIFIER_STRING.with(|i_string| {
-            // identifier : [a-zA-Z][a-zA-Z0-9]*
-            if last_char.is_alphabetic() {
-                let mut identifier_str = i_string.borrow_mut();
-                *identifier_str = last_char.to_string();
+        last_char_dup = *last_char;
+    });
 
-                loop {
-                    *last_char = input::get_char();
-                    if !last_char.is_alphabetic() {
-                        break;
-                    }
-                    identifier_str.push(*last_char);
-                }
+    detect_token = IDENTIFIER_STRING.with(|i_string| {
+        // identifier : [a-zA-Z][a-zA-Z0-9]*
+        if last_char_dup.is_alphabetic() {
+            let mut identifier_str = i_string.borrow_mut();
+            *identifier_str = last_char_dup.to_string();
 
-                if *identifier_str == "def" {
-                    return Token::Def;
+            loop {
+                last_char_dup = input::get_char();
+                if !last_char_dup.is_alphabetic() {
+                    break;
                 }
-                if *identifier_str == "extern" {
-                    return Token::Extern;
-                }
-                return Token::Identifier;
+                identifier_str.push(last_char_dup);
             }
-            Token::Invalid(' ')
+
+            if *identifier_str == "def" {
+                return Token::Def;
+            }
+            if *identifier_str == "extern" {
+                return Token::Extern;
+            }
+            return Token::Identifier;
+        }
+        Token::Uninitialized
+    });
+
+    // Number [0-9.]+
+    if last_char_dup.is_numeric() || last_char_dup == '.' {
+        let mut num_str = String::new();
+        loop {
+            num_str.push(last_char_dup);
+            last_char_dup = input::get_char();
+
+            if !last_char_dup.is_numeric() || last_char_dup == '.' {
+                break;
+            }
+        }
+        NUM_VALUE.with(|n_value| {
+            let mut num_value = n_value.borrow_mut();
+            *num_value = num_str.parse::<f64>().expect("Error parsing number!");
         });
+        detect_token = Token::Number;
+    }
+    // Comment
+    if last_char_dup == '#' {
+        loop {
+            last_char_dup = input::get_char();
 
-        if detect_token != Token::Invalid(' ') {
-            return detect_token;
-        }
-
-        // Number [0-9.]+
-        if last_char.is_numeric() || *last_char == '.' {
-            let mut num_str = String::new();
-            loop {
-                num_str.push(*last_char);
-                *last_char = input::get_char();
-
-                if !(last_char.is_numeric() || *last_char == '.') {
-                    break;
-                }
+            if last_char_dup == '\n' || last_char_dup == '\r' || last_char_dup == EOF as char {
+                break;
             }
-            NUM_VALUE.with(|n_value| {
-                let mut num_value = n_value.borrow_mut();
-                *num_value = num_str.parse::<f64>().expect("Error parsing number!");
+        }
+        
+        if last_char_dup != EOF as char {
+            LAST_CHAR.with(|l_char| {
+                let mut last_char = l_char.borrow_mut();
+                *last_char = last_char_dup;
             });
-            return Token::Number;
+            detect_token = get_tok();
         }
-        // Comment
-        if *last_char == '#' {
-            loop {
-                *last_char = input::get_char();
+    }
 
-                if *last_char == '\n' || *last_char == '\r' || *last_char == EOF as char {
-                    break;
-                }
+    if last_char_dup == EOF as char {
+        detect_token = Token::Eof;
+    }
+    if detect_token == Token::Uninitialized {
+        let this_char = Token::Invalid(last_char_dup);
+        last_char_dup = input::get_char();
+        detect_token = this_char;    
+    }
 
-                if *last_char != EOF as char {
-                    return get_tok();
-                }
-            }
-        }
-
-        if *last_char == EOF as char {
-            return Token::Eof;
-        }
-
-        let this_char = Token::Invalid(*last_char);
-        *last_char = input::get_char();
-        this_char
+    LAST_CHAR.with(|l_char| {
+        let mut last_char = l_char.borrow_mut();
+        *last_char = last_char_dup;
+        detect_token
     })
 }
 
@@ -106,7 +118,5 @@ mod tests {
     use super::*;
 
     #[test]
-    fn token_test() {
-        
-    }
+    fn token_test() {}
 }
--- src/utils/input.rs
+++ src/utils/input.rs
@@ -2,9 +2,10 @@ use std::io::Read;
 
 pub fn get_char() -> char {
     std::io::stdin()
-        .bytes() 
+        .bytes()
         .next()
         .and_then(|result| result.ok())
         .map(|byte| byte as char)
         .expect("Failed to read from stdout!")
 }
+
