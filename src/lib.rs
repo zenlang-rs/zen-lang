d1e9be5879f8d173a929c4bce98f33744fef5686
Lab Rat <35325046+rootCircle@users.noreply.github.com>
2023-10-17 21:54:24 +0000

    ops: created deploy using github actions

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 /// Just returns the same code as Ok!
 /// TODO implement later
-pub fn compile(code: String) -> Result<String, String> {
+pub fn compile(mut code: String) -> Result<String, String> {
+    code.push_str("\nBhag yha se!!\nOk?");
     Ok(code)
+}
+
+pub fn get_version() -> Result<String, &'static str> {
+    Ok(String::from("testv1"))
 }
